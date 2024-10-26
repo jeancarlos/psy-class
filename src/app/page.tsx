35180845f9a438d551d53902d27ba11c61d32e75
Jean Souza <jean.co.souza@gmail.com>
2024-10-26 19:02:43 -0300

    fix layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,29 +34,48 @@ export default function Home() {
       direction="column"
       alignItems="center"
       justifyContent="center"
-      sx={{ minHeight: '100vh' }}
+      sx={{ minHeight: '100vh', overflow: 'hidden' }}
     >
       <Box>
         <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-          <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+          <Tabs
+            className='tabs'
+            variant="scrollable"
+            scrollButtons
+            allowScrollButtonsMobile
+            value={value}
+            onChange={handleChange}
+            sx={{ maxWidth: '100vw' }}
+          >
+            <Tab label="Bem vindo" />
             <Tab label="Introdução" />
             <Tab label="Aula 1" />
             <Tab label="Aula 2" />
             <Tab label="Aula 3" />
           </Tabs>
         </Box>
-        <TabPanel value={value} index={0}>
-        <iframe width="1024" height="576" src="https://www.youtube.com/embed/5y7j0lRQbAI" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-        <iframe width="1024" height="576" src="https://www.youtube.com/embed/niXm6oYQArU" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-        </TabPanel>
-        <TabPanel value={value} index={2}>
-        <iframe width="1024" height="576" src="https://www.youtube.com/embed/mCWL-k3JE20"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-        </TabPanel>
-        <TabPanel value={value} index={3}>
-        <iframe width="1024" height="576" src="https://www.youtube.com/embed/Mo5c1jFU_ds" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-        </TabPanel>
+        <TabPanel value={value} index={0}><span className="aspect-box">
+          <Box className="boxIntro" sx={{ p: 3, top: '60px', maxWidth: 'calc(100% - 50px)', textAlign: 'justify', fontSize: '20px' }}>
+          Bem-vindo ao nosso curso sobre Pesquisa de Clima Organizacional!<br /><br />
+
+          Neste curso, você irá explorar as ferramentas e técnicas essenciais para avaliar e entender o ambiente de trabalho em qualquer organização.<br /><br />
+
+          A pesquisa de clima organizacional é crucial para identificar áreas de melhoria, promover a satisfação dos colaboradores e aumentar a produtividade.
+          Prepare-se para adquirir conhecimentos práticos e teóricos que vão transformar a forma como você percebe e gerencia a cultura organizacional.
+          Vamos começar essa jornada juntos e aprender a transformar ambientes de trabalho em espaços de excelência e colaboração!
+          </Box></span></TabPanel>
+        <TabPanel value={value} index={1}><span className="aspect-box">
+        <iframe width="896" height="504" src="https://www.youtube.com/embed/5y7j0lRQbAI" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        </span></TabPanel>
+        <TabPanel value={value} index={2}><span className="aspect-box">
+        <iframe width="896" height="504" src="https://www.youtube.com/embed/niXm6oYQArU" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        </span></TabPanel>
+        <TabPanel value={value} index={3}><span className="aspect-box">
+        <iframe width="896" height="504" src="https://www.youtube.com/embed/mCWL-k3JE20"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        </span></TabPanel>
+        <TabPanel value={value} index={4}><span className="aspect-box">
+        <iframe width="896" height="504" src="https://www.youtube.com/embed/Mo5c1jFU_ds" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        </span></TabPanel>
       </Box>
     </Grid>
   );
