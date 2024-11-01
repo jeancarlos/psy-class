569c7e6bda8263bc7dc602a034de45f64acb279c
Jean Souza <jean.co.souza@gmail.com>
2024-11-01 03:08:43 -0300

    remove styles

--- src/app/SubTitle.tsx
+++ src/app/SubTitle.tsx
@@ -4,22 +4,6 @@ import { Box, Typography } from '@mui/material';
 import NewReleasesIcon from '@mui/icons-material/NewReleases';
 
 export default function SubTitle(props: { children: React.ReactNode }) {
-  const sx = {
-    fontSize: '35px',
-    color: '#84CEC4',
-    position: 'absolute',
-    top: '-6px',
-    left: '0',
-    zIndex: 2
-  }
-  const sx2 = {
-    top: '-9px',
-    position: 'absolute',
-    color: '#000',
-    fontSize: '41px',
-    left: '0',
-    zIndex: 1
-  }
   return (
     <Typography variant="h3" component="h2" fontSize='18px' fontWeight={700} sx={{ mb: 3, display: 'flex', alignItems: 'center' }}>
       <Box className="subTitle subTitleIcon" component="span"><NewReleasesIcon /></Box>
